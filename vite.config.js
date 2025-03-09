deace14ffeab9b781848ba26ddd1d192cd90d89d
hokou <hokou.shiau@gmail.com>
2025-03-09 20:19:05 +0800

    git init

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/hex-react-2024-wk6/' : '/',
+  plugins: [react()],
+})
